d3e298b224631439bc27b833ba228fc3330dcb7d
kozyrrrr <kozyrstudy@gmail.com>
2021-12-18 15:40:12 +0300

    congig.js fix

--- config.js
+++ config.js
@@ -4,12 +4,26 @@ module.exports = {
     credentials: {
         client_id: process.env.FORGE_CLIENT_ID,
         client_secret: process.env.FORGE_CLIENT_SECRET,
-        callback_url: process.env.FORGE_CALLBACK_URL
+        callback_url: process.env.FORGE_CALLBACK_URL,
+        webhook_url: process.env.FORGE_WEBHOOK_URL
     },
     scopes: {
         // Required scopes for the server-side application
-        internal: ['bucket:create', 'bucket:read', 'data:read', 'data:create', 'data:write'],
+        internal: ['code:all','bucket:create', 'bucket:read', 'data:read', 'data:create', 'data:write', 'bucket:delete'],
         // Required scope for the client-side viewer
         public: ['viewables:read']
+    },
+    client: {
+        circuitBreaker: {
+            threshold: 11,
+            interval: 1200
+        },
+        retry: {
+            maxNumberOfRetries: 7,
+            backoffDelay: 4000,
+            backoffPolicy: 'exponentialBackoffWithJitter'
+        },
+        requestTimeout: 13000
     }
 };
+
